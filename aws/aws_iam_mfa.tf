18f74288054991aa44bc2c2df690e6c98c8e21e3
Todd Michael Bushnell <todd@strataconsulting.com>
2020-01-19 18:46:10 -0800

    use data resource for aws_account_id

--- aws/aws_iam_mfa.tf
+++ aws/aws_iam_mfa.tf
@@ -1,5 +1,7 @@
+data "aws_caller_identity" "current" {}
+
 locals {
-  aws_account = "account_number_goes_here"
+  aws_account = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_iam_policy" "enforce_mfa" {
